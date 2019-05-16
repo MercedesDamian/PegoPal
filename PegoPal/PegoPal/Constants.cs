8088b37c6f364217802164a3ff54905b870a92df
MercedesDamian <mercedes.damian@smail.rasmussen.edu>
2019-05-16 04:26:53 -0400

    v1.01
    Created Home screen
    Attempted to add database backend using Azure app services

---
+++ PegoPal/PegoPal/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PegoPal
+{
+    public static class Constants
+    {
+
+        //Azure Mobile App connection string
+        public static string ApplicationURL = @"https://pegopalbackend.azurewebsites.net";
+
+    }
+}

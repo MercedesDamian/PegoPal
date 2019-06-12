8a8f8cfe7c5904c593b256dbc2209b90a0c01f30
MercedesDamian <mercedes.damian@smail.rasmussen.edu>
2019-06-12 18:33:22 -0400

    V. 1.06
    Completed Recipes page
    Added in the kibble recipes
    icons and textboxes
    
    -Adjusted Creature list page
    -Adjusted Stat Calc page
    -Fixxed Mainpage button navigation
    
    **Creatures modifiers are still not functioning
    
    fully functioning as of 6/12/19

--- PegoPal/PegoPal/Recipes.xaml.cs
+++ PegoPal/PegoPal/Recipes.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -146,6 +147,299 @@ namespace PegoPal
                         break;
                 }
             }
+        }
+
+        //Combobox items
+        private void SimpleKibble_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+            //images & textboxes 
+            BitmapImage EggImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Dodo_Egg.png")
+            };
+            EggImage.Source = EggImageSource;
+            EggType.Text = "x1 Small Egg";
+
+            BitmapImage MeatImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Cooked_Fish_Meat.png")
+            };
+            MeatImage.Source = MeatImageSource;
+            MeatType.Text = "x1 Cooked Fish Meat";
+
+            BitmapImage VegImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Rockarrot.png")
+            };
+            VegImage.Source = VegImageSource;
+            VegType.Text = "x2 Rockarrot";
+
+            BitmapImage BerryImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Mejoberry.png")
+            };
+            BerryImage.Source = BerryImageSource;
+            BerryType.Text = "x5 Mejoberries";
+
+            BitmapImage FiberImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Fiber.png")
+            };
+            FiberImage.Source = FiberImageSource;
+            FiberType.Text = "x5 Fiber";
+
+            BitmapImage WaterImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Waterskin.png")
+            };
+            WaterImage.Source = WaterImageSource;
+            WaterType.Text = "Waterskin";
+
+        }
+
+        private void RegularKibble_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            //images & textboxes 
+            BitmapImage EggImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Dodo_Egg.png")
+            };
+            EggImage.Source = EggImageSource;
+            EggType.Text = "x1 Medium Egg";
+
+            BitmapImage MeatImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Cooked_Meat_Jerky.png")
+            };
+            MeatImage.Source = MeatImageSource;
+            MeatType.Text = "x1 Cooked Meat Jerky";
+
+            BitmapImage VegImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Longrass.png")
+            };
+            VegImage.Source = VegImageSource;
+            VegType.Text = "x2 Longrass";
+
+            BitmapImage BerryImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Savoroot.png")
+            };
+            BerryImage.Source = BerryImageSource;
+            BerryType.Text = "x2 Savoroot";
+
+            BitmapImage FiberImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Fiber.png")
+            };
+            FiberImage.Source = FiberImageSource;
+            FiberType.Text = "x5 Fiber";
+
+            BitmapImage WaterImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Waterskin.png")
+            };
+            WaterImage.Source = WaterImageSource;
+            WaterType.Text = "Waterskin";
+        }
+
+        private void SuperiorKibble_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            //images & textboxes 
+            BitmapImage EggImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Dodo_Egg.png")
+            };
+            EggImage.Source = EggImageSource;
+            EggType.Text = "x1 Large Egg";
+
+            BitmapImage MeatImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Prime_Meat_Jerky.png")
+            };
+            MeatImage.Source = MeatImageSource;
+            MeatType.Text = "x1 Prime Meat Jerky";
+
+            BitmapImage VegImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Citronal.png")
+            };
+            VegImage.Source = VegImageSource;
+            VegType.Text = "x2 Citronal";
+
+            BitmapImage BerryImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Sap.png")
+            };
+            BerryImage.Source = BerryImageSource;
+            BerryType.Text = "x1 Sap";
+
+            BitmapImage FiberImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Fiber.png")
+            };
+            FiberImage.Source = FiberImageSource;
+            FiberType.Text = "x5 Fiber";
+
+            BitmapImage WaterImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Rare_Mushroom.png")
+            };
+            WaterImage.Source = WaterImageSource;
+            WaterType.Text = "x2 Rare Mushroom";
+        }
+
+        private void ExceptionalKibble_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            //images & textboxes 
+            BitmapImage EggImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Dodo_Egg.png")
+            };
+            EggImage.Source = EggImageSource;
+            EggType.Text = "x1 Extra Large Egg";
+
+            BitmapImage MeatImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Focal_Chili.png")
+            };
+            MeatImage.Source = MeatImageSource;
+            MeatType.Text = "x1 Focal Chili";
+
+            BitmapImage VegImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Rare_Flower.png")
+            };
+            VegImage.Source = VegImageSource;
+            VegType.Text = "x1 Rare Flower";
+
+            BitmapImage BerryImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Mejoberry.png")
+            };
+            BerryImage.Source = BerryImageSource;
+            BerryType.Text = "x10 Mejoberries";
+
+            BitmapImage FiberImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Fiber.png")
+            };
+            FiberImage.Source = FiberImageSource;
+            FiberType.Text = "x5 Fiber";
+
+            BitmapImage WaterImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Waterskin.png")
+            };
+            WaterImage.Source = WaterImageSource;
+            WaterType.Text = "Waterskin";
+        }
+
+        private void ExtraordinaryKibble_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            //images & textboxes 
+            BitmapImage EggImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Dodo_Egg.png")
+            };
+            EggImage.Source = EggImageSource;
+            EggType.Text = "x1 Special Egg";
+
+            BitmapImage MeatImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Giant_Bee_Honey.png")
+            };
+            MeatImage.Source = MeatImageSource;
+            MeatType.Text = "x1 Giant Bee Honey";
+
+            BitmapImage VegImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Lazarus_Chowder.png")
+            };
+            VegImage.Source = VegImageSource;
+            VegType.Text = "x1 Lazarus Chowder";
+
+            BitmapImage BerryImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Mejoberry.png")
+            };
+            BerryImage.Source = BerryImageSource;
+            BerryType.Text = "x10 Mejoberries";
+
+            BitmapImage FiberImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Fiber.png")
+            };
+            FiberImage.Source = FiberImageSource;
+            FiberType.Text = "x5 Fiber";
+
+            BitmapImage WaterImageSource = new BitmapImage
+            {
+                // Call BaseUri on the root Page element and combine it with a relative path
+                // to consruct an absolute URI.
+                UriSource = new Uri(this.BaseUri, "https://www.dododex.com/media/item/Waterskin.png")
+            };
+            WaterImage.Source = WaterImageSource;
+            WaterType.Text = "Waterskin";
         }
         //end of code
     }
